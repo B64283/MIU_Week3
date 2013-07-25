d580f9e5386ab8f7d2426443700d67b0bcf35260
B64283 <mattdarke@fullsail.edu>
2013-07-25 05:00:23 -0700

    Finally at 4:59 am it displays error messages

--- GoldApp/js/main.js
+++ GoldApp/js/main.js
@@ -1,20 +1,38 @@
+
 $('#home').on('pageinit', function(){
 	//code needed for home page goes here
-});	
+ });	
 		
 $('#addItem').on('pageinit', function(){
 
-		var myForm = $('#formId');
-		    myForm.validate({
+		var myForm = $('#travelForm');
+		    tterrorsLink	= $ ("#tterrorsLink");	    
+		    
+		myForm.validate({
 			invalidHandler: function(form, validator) {
+			     tterrorsLink.click();
+			     for(var key in validator.submitted){
+			       var label =  $(' label[for^=" ' + key +' "]') 
+			     };
 			},
 			submitHandler: function() {
-		var data = myForm.serializeArray();
+		var data = travelForm.serializeArray();
 			storeData(data);
+			var item			  = {}; 
+	        item.TravelType    =["TravelType:", $("TravelType").value];         
+	        item.Destination    =["Destination:", $( "Destination").value];
+	        item.clothes        =["Clothes:", $("Clothes").value];
+	        item.meds          =["Medication:", $("Medication").value];
+	        item.toiletries       =["Toiletries:", $("Toiletries").value];	        
+	        item.Duration       =["Duration:", $("Duration").value];	        
+	        item.slider1         =["slider1:" , $("slider1").value];
+	        item.startdate       =["startdate:", $("startdate").value];	        
+	        item.notes          =["notes:", $("notes").value];
+ 	   	   localStorage.setItem(id, JSON.stringify(item) );
+	   alert("Checklist Complete!");
 		}
 	});
-	
-	//any other code needed for addItem page goes here
+		//any other code needed for addItem page goes here
 	
 });
 
@@ -29,7 +47,7 @@ var getData = function(){
 };
 
 var storeData = function(data){
-	
+
 }; 
 
 var	deleteItem = function (){
@@ -37,6 +55,6 @@ var	deleteItem = function (){
 };
 					
 var clearLocal = function(){
-
+   
 };
 
